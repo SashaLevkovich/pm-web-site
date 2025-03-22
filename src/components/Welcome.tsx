099d7627db1b7a50bc4b69a9468e8ac99c903472
aleksandr-levkovich-gismart <alexander.levkovich@gismart.com>
2025-03-22 11:37:51 +0300

    [PM-1] Init project & add dependencies

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useStore } from "../store/store";
+
+export const WelcomeComponent = () => {
+  const { count, increment, decrement } = useStore();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Счётчик: {count}</h1>
+      <div className="flex flex-row gap-2">
+        <button
+          onClick={increment}
+          className="bg-blue-500 text-white p-2 rounded-md">
+          +
+        </button>
+        <button
+          onClick={decrement}
+          className="bg-blue-500 text-white p-2 rounded-md">
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
